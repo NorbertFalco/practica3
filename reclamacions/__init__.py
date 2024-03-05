e631a29f5478054889bcef17c98e4bc144349e6a
Norbert Falcó <norbertfalco@gmail.com>
2024-03-05 16:58:09 +0100

    arreglo el nom

---
+++ reclamacions/__init__.py
@@ -0,0 +1,2 @@
+from . import models
+from . import reclamacions_property
---
+++ reclamacions/models/reclamacions_property.py
@@ -0,0 +1,5 @@
+from odoo import fields, models
+class ReclamacionsProperty(models.Model):
+    _name = 'reclamacions.property'
+
+
