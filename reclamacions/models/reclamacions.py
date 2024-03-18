b71b0bde2a2a43dcdca11d88f6f16a6d4435538f
Norbert Falcó <norbertfalco@gmail.com>
2024-03-18 20:00:49 +0100

    arreglat el commit del norbert

--- reclamacions/models/reclamacions.py
+++ reclamacions/models/reclamacions.py
@@ -34,42 +34,20 @@ class Reclamacions(models.Model):
 
     # el nombre d'enviaments associats a la comanda
     delivery_count = fields.Integer(compute='_compute_delivery_count', string='Nombre d\'Enviaments')
+    picking_count = fields.Integer(compute='_compute_picking_count', string='Nombre d\'Enviaments')
 
-    # el nombre de factures associades a la comanda
-    @api.depends('sale_order_id.invoice_ids')
-    def _compute_invoice_count(self):
-        for record in self:
-            record.invoice_count = len(record.sale_order_id.invoice_ids)
-
-    # el nombre d'enviaments associats a la comanda
-    @api.depends('sale_order_id.picking_ids')
-    def _compute_delivery_count(self):
-        for record in self:
-            record.delivery_count = len(record.sale_order_id.picking_ids)
 
     # l'estat de la reclamació
-    status = fields.Selection([
+    state = fields.Selection([
         ('nova', 'Nova'),
         ('en_tractament', 'En Tractament'),
         ('tancada', 'Tancada'),
         ('cancellada', 'Cancel·lada')
-    ], string='Status', default='draft')
+    ], string='State', default='draft')
 
     # la resolució de la reclamació
     resolution_description = fields.Text(string='Descripció de la resolució')
 
-    # faltarà decidir quins motius de tancament i cancel·lació volem
-    closure_reason = fields.Selection([
-        ('reason1', 'Reason 1'),
-        ('reason2', 'Reason 2'),
-        ('reason3', 'Reason 3')
-    ], string='Motiu de tancament')
-
-    cancellation_reason = fields.Selection([
-        ('reason1', 'Reason 1'),
-        ('reason2', 'Reason 2'),
-        ('reason3', 'Reason 3')
-    ], string='Motiu de cancel·lació')
 
     # els missatges associats a la reclamació
     missatges_ids = fields.One2many('missatges', 'reclamacio_id', string='Missatges')
