f6d1ab85a939fe1283feee86bc7afc6af1f3e2a4
Norbert Falcó <norbertfalco@gmail.com>
2024-05-14 19:17:24 +0200

    ara s'instal·la

--- reclamacions/__manifest__.py
+++ reclamacions/__manifest__.py
@@ -5,15 +5,16 @@
     "depends": ["base", "mail", "sale", 'account', 'stock'],
     "data": [
         'security/ir.model.access.csv',
-        'views/llista_reclamacions_views.xml',
-        'views/reclamacions_menu.xml',
-        'views/formulari_reclamacions.xml',
-        'views/motiu.xml',
         'views/closing_reason_views.xml',
-        'views/venda_views.xml',
         'views/email_template_order_cancellation.xml',
+        'views/formulari_reclamacions.xml',
+        'views/llista_reclamacions_views.xml',
         'views/motiu_cancelar.xml',
+        'views/motiu.xml',
+        'views/reclamacions_menu.xml',
+        'views/venda_views.xml',
 
+        
     ],
     "installable": True,
     'license': 'LGPL-3',
